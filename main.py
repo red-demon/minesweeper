a5048072862320a79df2a57f1217b039fb3f11cc
red-demon <rafael@bonafini.com>
2022-04-27 22:14:24 +0200

    "settings"

--- main.py
+++ main.py
@@ -8,19 +8,27 @@ This is a temporary script file.
 #%%
 
 import tkinter as tk #tkinter is a GUI-Library for python
+import settings
 
-#%%
-width=720
-height=400
+
+#%% initialize window
 root = Tk() #initialize root window (would also bei initialized once I create first widget)
 root.configure(bg="black")
 root.geometry(str(width)+'x'+str(height)) #create size for root window
 root.title('Minesweeper') #create title for root window
 root.resizable(False, False) #prohibit resizing of width and height of window
+
 top_frame=Frame(
         root,
         bg='red',
         width=width,
         height=height/4) #create the topframe
 top_frame.place(x=0,y=0) #place top frame at coordinates 0,0
+
+left_frame=Frame(
+        root,
+        bg='blue',
+        width=width/4,
+        height=height*3/4)
+left_frame.place(x=0,y=height/4)
 root.mainloop()
---
+++ settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 22:10:52 2022
+
+@author: Rafael
+"""
+
+#%% define variables
+width=720
+height=400
